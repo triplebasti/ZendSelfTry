d0585a0ed54265c58ec618bcf643ff2fa1544e28
triplebasti <S.krebs91@web.de>
2014-03-14 11:20:26 +0100

    entwicklung

--- module/HelloWorld/config/module.config.php
+++ module/HelloWorld/config/module.config.php
@@ -3,21 +3,32 @@ return array(
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
-               ),
-            ),
+         ),
+        'strategies' => array('ViewJsonStrategy')
+    ),
     'router' => array(
         'routes' => array(
             'sayhello' => array(
                 'type' => 'Zend\Mvc\Router\Http\Literal',
-                    'options' => array(
-                        'route'=> '/sayhello',
-                        'defaults' => array(
-                            'controller' => 'Helloworld\Controller\Index',
-                             'action'=> 'index',
-                        )
-                    )
-            )
-        )
+                'options' => array(
+                    'route'    => '/sayhello',
+                    'defaults' => array(
+                        'controller' => 'Helloworld\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            'show' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/show',
+                    'defaults' => array(
+                        'controller' => 'Helloworld\Controller\Index',
+                        'action'     => 'show',
+                    ),
+                ),
+            ),
+        ),
     ),
     'controllers' => array(
         'invokables' => array(
--- module/HelloWorld/src/Helloworld/Controller/IndexController.php
+++ module/HelloWorld/src/Helloworld/Controller/IndexController.php
@@ -3,14 +3,24 @@ namespace Helloworld\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\View\Model\JsonModel;
 
 class IndexController extends AbstractActionController
 {
 
     public function indexAction()
     {
-
         return new ViewModel();
+    }
+
+    public function showAction()
+    {
+        $returnArray = array();
+        $post = $this->getRequest()->getPost();
+        if (isset($post['data'])) {
+            $returnArray['Information'] = 'Es geht oder so!';
+        }
+        return new JsonModel($returnArray);
     }
 
 }
